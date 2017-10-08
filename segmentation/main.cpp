c6d8f25567b93476b695bfb08343a179f77b5b2f
ARQtty <mr.bookmix@gmail.com>
2017-10-08 12:47:23 +0300

    add main()

--- segmentation/main.cpp
+++ segmentation/main.cpp
@@ -2,7 +2,14 @@
 #include <cmath>
 #include <unordered_map>
 #include <vector<>
-//#include <GL/glut.h>
+#include <iostream>
+#include <vector>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/ml.hpp"
+#include "opencv2/objdetect.hpp"
 using namespace std;
 
 const int Q_PICTURES = 1; // Количество обрабатываемых пикч
@@ -10,14 +17,12 @@ const int DOWNSIZE_K = 4; // Коэф. уменьш. пикч
 const string HUE_CUTTED_IMG_PATH = "./data/HUE_cutted/";
 const string ORIGINAL_IMG_PATH = "./data/originals";
 
-/*
-double diff_grey(imgType___ img, int x1, int y1, int x2, int y2){
+
+double diff_grey(Mat img, int x1, int y1, int x2, int y2){
    // Эвристика расстояния на greyscale пикчах
    double v = abs(img[x1, y1] - img[x2, y2]);
    return v
 };
-*/
-
 
 unordered_map groupByParent(Forest forest, int width, int height){
    vector<pair<int, int>> v;
@@ -57,6 +62,18 @@ vector<int, int, int, int> getRectsFromGroups(unordered_map groups, int imWidht,
 };
 
 
-int main(){
-   
+
+
+int main() {
+    cv::Mat img = cv::imread("test.jpg");
+    cv::Mat hsv;
+    cv::cvtColor(img,hsv,cv::COLOR_BGR2HSV);
+    cv::Mat mask;
+    cv::inRange(hsv, {90, 120, 0}, {180, 200, 255}, mask);
+    cv::inRange(hsv, {0, 120, 80}, {40, 255, 255}, mask);
+    cv::Mat res;
+    bitwise_and(res, res, res, mask);
+    cv::imwrite("res.jpg", res);
+
+    return 0;
 }
